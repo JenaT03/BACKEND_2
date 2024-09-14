45cc18f5242d90ce232b837a1993d0c0732a9d18
Nhi <nhi19603@gmail.com>
2024-09-14 14:35:49 +0700

    Cai dat handler create

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,3 +1,7 @@
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.util");
+const ApiError = require("../api-error");
+
 exports.create = (req, res) => {
     res.send({message : "create handler"});
 }
@@ -24,4 +28,20 @@ exports.deleteAll = (req, res) => {
 
 exports.findAllFavorite = (req, res) => {
     res.send({message: "findAllFavorite"});
+}
+
+exports.create = async (req, res, next) => {
+    if(!req.body?.name) { // req tại body không chứa name
+        return next(new ApiError(400, "Name can not empty")); // next() chuyển tới middleware để xử lý lỗi
+    }
+
+    try{
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.create(req.body);
+        return res.send(document);
+    }catch(error) {
+        return next(
+            new ApiError(500, "An error occured while creating the contact")
+        );
+    }
 }
--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -1,8 +1,33 @@
-const {ObjectId} = require("mongo");
+const {ObjectId} = require("mongodb");
 
-class ConatctService {
+class ContactService {
     constructor(client){
         this.Contact = client.db().collection("contacts");
+    }
+
+    extractCotactData(payload) { 
+        const contact = { // contact được tạo ra từ các thuộc tính trong payload
+            name : payload.name,
+            email: payload.email,
+            address: payload.address,
+            phone: payload.phone,
+            favorite: payload.favorite,
+        };
+
+        Object.keys(contact).forEach( // lọc dữ liệu, nếu có value là undefined thì xóa nó
+            (key)=> contact[key] === undefined && delete contact[key]
+        );
+        return contact;
+    }
+    async create(payload) { //payload là req chứa thông tin contact
+        const contact = this.extractCotactData(payload);
+        const result = await this.Contact.findOneAndUpdate(
+            contact,
+            {$set: {favorite:contact.favorite === true}},  // cập nhật atr fav là true nếu giá trị contact.fav là true
+            {returnDocument: "after", upsert: true} // upsert cho phép tạo doc mới nếu ko tồn tại, after đảm bảo kết quả trả về sau cập nhật
+
+        );
+        return result;
     }
 }
 
