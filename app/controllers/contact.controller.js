cbdfdeb3b9d2c3c594eee7e6d33640d8c17aceb2
Nhi <nhi19603@gmail.com>
2024-09-14 19:29:07 +0700

    Cai dat handler findOne va update

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -2,21 +2,6 @@ const ContactService = require("../services/contact.service");
 const MongoDB = require("../utils/mongodb.util");
 const ApiError = require("../api-error");
 
-exports.findOne = (req, res) => {
-    res.send({message: "findOne handler"});
-}
-
-exports.update = (req, res) => {
-    res.send({message: "update handler"});
-}
-
-exports.delete = (req, res) => {
-    res.send({message: "delete handler"});
-}
-
-exports.deleteAll = (req, res) => {
-    res.send({message: "deleteAll handler"});
-}
 
 exports.findAllFavorite = (req, res) => {
     res.send({message: "findAllFavorite"});
@@ -56,4 +41,37 @@ exports.findAll = async (req, res, next) => {
         )
     }
     return res.send(documents);
-}
+};
+
+exports.findOne = async(req, res, next) => {
+    try{
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.findById(req.params.id); // lấy id trên method get để tìm
+        if (!document) {
+            return next(new ApiError(404, "Contact not found"));
+        }
+        return res.send(document);
+    }catch(error){
+        return next(
+            new ApiError(500, `Error retrieving contact with id =${req.params.id}`)
+        );
+    }
+};
+
+exports.update = async(req, res, next) => {
+    if(Object.keys(req.body).length === 0) {
+        return next(new ApiError(400, "Data to update can not be empty"));
+    }
+    try{
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.update(req.params.id, req.body); // tìm id và update với dữ liệu từ req.body
+        if(!document){
+            return next(new ApiError(404, "Contact not found"));
+        }
+        return res.send({message: "Contact was updated sucessfully"});
+    }catch(error){
+        return next(
+            new ApiError(500, `Error updating contact with id = ${req.params.id}`)
+        );
+    }
+};
--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -40,6 +40,25 @@ class ContactService {
             name: { $regex: new RegExp(name), $options: "i"}, //toán tử regex-trong MongoDB cho phép tìm doc mà name chứa chuổi con khớp với biểu thức chính quy được tạo bởi new RegExp(name), op i ko phân biệt hoa thường
         });
     }
+
+    async findById(id) {
+        return await this.Contact.findOne({
+            _id: ObjectId.isValid(id)? new ObjectId(id) : null, // kiểm tra xem id có hợp lệ hay không nếu có nó sẽ tạo ra và tìm kiếm bằng _id
+        });
+    }
+
+    async update(id, payload){
+        const filter = {
+            _id: ObjectId.isValid(id)? new ObjectId(id) : null, 
+        };
+        const update = this.extractCotactData(payload);
+        const result = await this.Contact.findOneAndUpdate(
+            filter,
+            { $set: update }, // chỉ cập nhật các doc trong updtate
+            { returnDocument: "after" }
+        );
+        return result; // chứa doc được update success còn không thì là null
+    }
 }
 
 module.exports = ContactService;
